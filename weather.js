6369485090f7b69871b784e329bb9122b6ef4143
NEHAISRANI <nehai18@navgurukul.org>
2020-06-02 12:06:01 +0530

    in this task first of all i have require readline sync for taking the user input.have made api data one function for getting the data from weather api.and second function for get the city data

--- weather.js
+++ weather.js
@@ -1,21 +1,23 @@
 const raw_input=require('readline-sync').question;
-const cityInput=raw_input("enter the city name...:");
-const fs=require('fs')
+const cityInput=raw_input("enter the city name....:")
 var bodyParser=require('body-parser');
 
-function weatherApi(data){
+function apiData(weatherApi){
     const axios=require('axios');
-    return response=axios.get(data);
-}
-var url="https://api.openweathermap.org/data/2.5/weather?q="+"Mumbai"+"&appid=564b520ffae9528ec61ea1c1811d21e7"
-response=weatherApi(url)
+    return response=axios.get(weatherApi);
+}   
+var url="https://api.openweathermap.org/data/2.5/weather?q="+cityInput+"&appid=564b520ffae9528ec61ea1c1811d21e7"
+response=apiData(url);
 
-const getdata=(async(apiResponse)=>
-{
-    let data1=await(apiResponse)
-    
-
-})
-getdata(response) 
-
-
+function getCityData(data){
+    response.then((data)=>{
+        var insideData=data["data"]
+        var latAndLon=insideData["coord"];
+        var lat=latAndLon["lat"]
+        var lon=latAndLon["lon"]
+        var url1="https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=564b520ffae9528ec61ea1c1811d21e7"
+        var latAndLonData=apiData(url1) 
+        return(latAndLonData)
+    }) 
+}   
+getCityData(response)
